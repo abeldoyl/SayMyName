06e6c22dc2f7f2d69b5b259a20a9a7a4f4d365ac
abeldoyl <abeldoyle71@gmail.com>
2026-01-21 17:30:00 -0700

    added full header and console read and write funtions to ask user for name with response

--- SayMyName/Program.cs
+++ SayMyName/Program.cs
@@ -3,7 +3,7 @@ Abel Doyle
 2265
 Spring Semester 2026
 SayMyName
-
+https://github.com/abeldoyl/SayMyName.git
 */
 
 namespace SayMyName
@@ -12,9 +12,11 @@ namespace SayMyName
     {
         static void Main(string[] args)
         {
+            string userInput = "name";
+
             Console.WriteLine("What's your Name");
-
-
+            userInput = Console.ReadLine();
+            Console.WriteLine($"Hello {userInput}");
 
             Console.Read();
         }
