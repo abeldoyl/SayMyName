6297443570477db490e4cef4ba9a4493057380e1
abelc <abelc@ABELPC>
2026-01-21 17:15:30 -0700

    Add project files.

---
+++ SayMyName/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Abel Doyle
+2265
+Spring Semester 2026
+SayMyName
+
+*/
+
+namespace SayMyName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your Name");
+
+
+
+            Console.Read();
+        }
+    }
+}
